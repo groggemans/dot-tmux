4f00ad93e137baf77fd6689b278c197d2a7c6393
G. Roggemans <g.roggemans@grog.be>
2016-01-02 18:41:26 +0100

    Implemented install and pull hooks for ellipsis

--- ellipsis.sh
+++ ellipsis.sh
@@ -16,3 +16,27 @@ pkg.link() {
 }
 
 ##############################################################################
+
+pkg.install() {
+    # Install TPM
+    mkdir -p ~/.tmux/plugins
+    cd ~/.tmux/plugins
+    git.clone https://github.com/tmux-plugins/tpm
+    exec ~/.tmux/plugins/tpm/tpm
+    exec ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+}
+
+##############################################################################
+
+ pkg.pull() {
+    # Update dot-tmux repo
+    git.pull
+
+    # Update plugins
+    exec ~/.tmux/plugins/tpm/tpm
+    exec ~/.tmux/plugins/tpm/scripts/clean_plugins.sh
+    exec ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+    exec ~/.tmux/plugins/tpm/scripts/update_plugin.sh
+ }
+
+##############################################################################
